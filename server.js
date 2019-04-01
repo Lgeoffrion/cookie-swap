07e19fd5f78ef693f18a1497bd8384bde35c3a5b
Logan Geoffrion <45701086+Lgeoffrion@users.noreply.github.com>
2019-04-01 14:29:02 -0500

    removed model.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var session = require("express-session");
 // Requiring passport as we've configured it
 // var passport = require("./config/passport");
 const routes = require("./routes");
-var db = require("./models/user");
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,6 +16,7 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
 app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
 // app.use(passport.initialize());
 // app.use(passport.session());
@@ -25,12 +26,6 @@ app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true 
 app.use(routes);
 
 
-// Start the API server
-// db.sequelize.sync({ force: false }).then(function () {
-//     app.listen(PORT, function () {
-//       console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
-//     });
-// });
 
 app.listen(PORT, function () {
   console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
