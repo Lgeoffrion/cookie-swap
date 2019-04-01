c2a14511c003eaa8533ec441e7f1c13d2d4d545a
Shankarri <shanki.ranju@gmail.com>
2019-04-01 13:15:30 -0500

    Front End to Backend link

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const express = require("express");
-
-const mongoose = require("mongoose");
+var express = require("express");
+var session = require("express-session");
+// Requiring passport as we've configured it
+var passport = require("./config/passport");
+// const mongoose = require("mongoose");
 const routes = require("./routes");
+var db = require("./models");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -12,13 +16,18 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 // Add routes, both API and view
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+//mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 // Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+db.sequelize.sync({ force: false }).then(function () {
+    app.listen(PORT, function () {
+      console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
+    });
 });
