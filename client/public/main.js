336ac2838af3c822c81031d4c54cd14022b14c43
Josh VanOverbeke <joshvanoverbeke@gmail.com>
2019-04-02 14:25:27 -0500

    working materialize js

---
+++ client/public/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    // var instance = M.Dropdown.getInstance(elem);
+    $('.sidenav').sidenav();
+    $('.dropdown-trigger').dropdown();
+    $('.collapsible').collapsible();
+    $('.tabs').tabs();
+});
