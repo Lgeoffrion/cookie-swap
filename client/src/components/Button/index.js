99cac2e2cbe422e232ca576646b5bc00975777fa
Shankarri <shanki.ranju@gmail.com>
2019-04-04 12:18:29 -0500

    Login Validation from DB

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -4,9 +4,13 @@ import "./style.css";
 
 function Button(props) {
     return (
+        <>
         <div className="buttonDiv">
-            <a className="waves-effect waves-light btn-large" {...props} href={props.ahref}>Login</a>
+            <a className="waves-effect waves-light btn-large" {...props} ahref={props.href}>Login</a>
+           
         </div>
+        <br/><span className="errorMsg">{props.errorMsg}</span>
+        </>
     );
 }
 
