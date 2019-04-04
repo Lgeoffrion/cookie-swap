69d07b0f5900cea598c24e1aea3c33b6f9bd0e07
Shankarri <shanki.ranju@gmail.com>
2019-04-03 23:43:11 -0500

    Login page validations and profile page setup

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -5,7 +5,7 @@ import "./style.css";
 function Button(props) {
     return (
         <div className="buttonDiv">
-            <a className="waves-effect waves-light btn-large" {...props} >Login</a>
+            <a className="waves-effect waves-light btn-large" {...props} href={props.ahref}>Login</a>
         </div>
     );
 }
