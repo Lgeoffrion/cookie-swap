ba7af02a2934e687e8ff066f1ce673bfd4f19a07
chong <chongvlee@hotmail.com>
2019-04-05 08:03:23 -0500

    clean up conflicts

--- server.js
+++ server.js
@@ -29,22 +29,10 @@ require("./routes/sum.js")(app);
 
 require("./routes/login.js")(app);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-// Add routes, both API and view
-app.use(routes);
-
-=======
->>>>>>> 2d5517286f8a4b1eaec24e8f17ae5f69396306c6
->>>>>>> b43c130a67e970d6bb228c2ecb1a4a2a97d3ce9a
-
-db.sequelize.sync().then(function() {
-=======
 // Add routes, both API and view
 app.use(routes);
 
 db.sequelize.sync({ force: false }).then(function () {
->>>>>>> 27a0c7d4126c3ab7e8f56ac36af83bd4636813e9
   app.listen(PORT, function() {
     console.log("App now listening on port:", PORT);
   });
