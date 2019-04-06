2f21a0e1c56153c32fa8ac03dcd8beac2862a2e4
Shankarri <shanki.ranju@gmail.com>
2019-04-05 23:30:59 -0500

    Login authentication and comments

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -13,32 +13,44 @@ class LoginPage extends Component {
     password: "",
     errorMsg:""
   };
+
+  // update the state while typing Email and Password fields  
   handleInputChange = event => {
     const { id, value, className } = event.target;
     this.setState({ [id]: value });
   };
 
+  // As soon as we go to Login page, clear the user Info if there is any
   componentDidMount() {
+    sessionStorage.removeItem('TCM_userInfo');
+    sessionStorage.removeItem('SUM_userInfo');
+    sessionStorage.clear();
     // this.loadBooks();
   }
 
   handleFormSubmit = () => {
     // event.preventDefault();
+    // as soon as submit button is clicked, clear the email and password field 
     this.setState({ email : "", password : "", });
+    
+    // When submit button is clicked and title is selected as "SUM", clear the email and password field 
     if (this.state.title === 'SUM') {
+      // Send in API call for checking the SUM Login and Password
       API.validateSUMLogin({
         email: this.state.email,
         password: this.state.password
       })
         .then(
           res => {
+            // Once we get the response check if we get the response Data Success as "yes"
             console.log("res.success", res.data.success);
             if (res.data.success === "Yes") {
-              sessionStorage.removeItem('userInfo');
+              sessionStorage.removeItem('SUM_userInfo');
               sessionStorage.clear();
-              sessionStorage.setItem('userInfo', JSON.stringify(res.data));
+              sessionStorage.setItem('SUM_userInfo', JSON.stringify(res.data));
               document.location.href = "/SUM";
             }
+            // If the response is not success then show Error Message
             else {
               this.setState({ errorMsg: "Invalid Username or Password" });
             }
@@ -46,23 +58,24 @@ class LoginPage extends Component {
         )
         .catch(err => console.log(err));
     }
+    // When submit button is clicked and title is selected as "TCM", clear the email and password field 
     else if (this.state.title === 'TCM') {
+       // Send in API call for checking the TCM Login and Password
       API.validateTCMLogin({
         email: this.state.email,
         password: this.state.password
       })
         .then(
           res => {
+             // Once we get the response check if we get the response Data Success as "yes"
             console.log("res.data.success", res.data.success);
-            // if (res.data.success ) {
-            //   this.setState({ errorMsg: "Invalid Username or Password" });
-            // }
             if(res.data.success === "Yes")   {
-              sessionStorage.removeItem('userInfo');
+              sessionStorage.removeItem('TCM_userInfo');
               sessionStorage.clear();
-              sessionStorage.setItem('userInfo', JSON.stringify(res.data.user));
+              sessionStorage.setItem('TCM_userInfo', JSON.stringify(res.data.user));
               document.location.href = "/TCM";
             }
+             // If the response is not success then show Error Message
             else
             {
               this.setState({ errorMsg: "Invalid Username or Password" });
@@ -73,22 +86,26 @@ class LoginPage extends Component {
     }
   };
 
-
   render() {
     return (
       <div>
+        {/* Passing the Navbar title and page Name as parameter */}
         <Navbar title={'Girl Scout Cookie Swap'}
           ahref={'/'}
           page={'login'} />
+
         <div className='row'>
           <div className="col col l7 push-l4 s12">
-
+          
+          {/* Dropdown for selecting Login for SUM / TCM */}
             <Dropdown
               value={this.state.title}
               id="title"
               name="Title"
               onChange={this.handleInputChange}
             />
+
+            {/* Seperate EmailInput compenent with Email validations */}
             <EmailInput
               value={this.state.email}
               onChange={this.handleInputChange}
@@ -101,6 +118,8 @@ class LoginPage extends Component {
                   this.state.email.trim().match(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/)) ? "valid" : "invalid"
               }
             />
+
+            {/* As of now no password validations*/}
             <PasswordInput
               value={this.state.password}
               onChange={this.handleInputChange}
@@ -108,10 +127,13 @@ class LoginPage extends Component {
               name="Password"
               placeholder="Password (required)"
             />
+
+            {/* Disable the Login button if email/password/ title is not entered and also if the email is not valid*/}
            <Button
               disabled={ !(this.state.email && this.state.title && this.state.password
                 && this.state.email.trim().match(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/))
               }
+              // Handle the login submission
               onClick={this.handleFormSubmit}
               error={this.state.errorMsg}
             />
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -5,41 +5,61 @@ import axios from "axios";
 // ***********
 
 export default {
-  // Gets all saved books
+  // get TCMS user information with the matching Id
   getYourInventory: function(id) {
     console.log("/api/tcms/"+ id)
     return axios.get("/api/tcms/"+ id);
   },
+
+  // get all the TCMS information from Db
   getTCMS: function(){
     return axios.get("/api/tcms");
   },
+
+  // get All Open Trades from the Trade
   getOpenSwaps: function(){
     return axios.get("/api/trades");
   },
+
+  // send in Sum Email id and Password to check for Login validation
   validateSUMLogin : function(userInfo)
   {
     return axios.post("/api/sum/login",userInfo);
   },
+
+  // send in TCM Email id and Password to check for Login validation
   validateTCMLogin : function(userInfo)
   {
     return axios.post("/api/tcm/login",userInfo);
   },
+
+  // Once request is made, create Trade in trade table 
   createTrade: function(newTrade)
   {
     return axios.put("/api/claim", newTrade)
   },
+
+  // Add a new TCM to the TCM table
   tcmCreate : function(tcm) {
     return axios.post("/api/addtcm",tcm)
   },
+
+  // Add the number of cookies in the inventory
   addCookies: function(cookies) {
     return axios.put("/api/add", cookies)
   },
+
+  // Remove number of Cookies in the Inventory
   subCookies: function(cookies) {
     return axios.put("/api/sub", cookies)
   },
+
+  // Once Request is made to swap cookies, take data from TCM table and update Trade table 
   swapCookies: function(swap) {
     return axios.post("api/offer", swap)
   },
+
+    // Once request is made, create Trade in trade table 
   claimCookies: function(claim) {
     return axios.post("api/claim", claim)
   }
